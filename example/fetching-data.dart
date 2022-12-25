9bd085c4ac887a25f176f07c211825c590dff924
Ashutosh <ashutoshparmar2202@gmail.com>
2022-12-25 15:56:50 +0530

    Retrieve Formdata in POST request

--- example/fetching-data.dart
+++ example/fetching-data.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
+
+
 Future<void> main() async {
   final server = await createServer();
   print('Server started: ${server.address} port ${server.port}');
@@ -40,69 +42,64 @@ void handleGet(HttpRequest req) async {
     ..write(myStringStorage)
     ..close();
 
-//     req.response.headers.contentType = ContentType.json;
-//     //CORS Header, so the anybody can use this
-//     req.response.headers.add(
-//       'Access-Control-Allow-Origin',
-//       '*',
-//       preserveHeaderCase: true,
-//     );
-
-//     try {
-//       final offset =
-//           int.parse(req.requestedUri.queryParameters['offset'] ?? '0');
-//       final pageSize =
-//           int.parse(req.requestedUri.queryParameters['pageSize'] ?? '10');
-//       final sortIndex =
-//           int.parse(req.requestedUri.queryParameters['sortIndex'] ?? '1');
-//       final sortAsc =
-//           int.parse(req.requestedUri.queryParameters['sortAsc'] ?? '1') == 1;
-// // ... sending data in the for loop
-
-//       // fileContent.sort((a, b) => sortContacts(a, b, sortIndex, sortAsc));
-//       req.response.write(
-//         jsonEncode(
-//            myStringStorage
-//         ),
-//       );
-
-
-//        } catch (e) {
-//       print('Something went wrong: $e');
-//       req.response.statusCode = HttpStatus.internalServerError;
-//     }
-//     // print(req)
-//     await req.response.close();
-
-
 }
+
+
 
 Future<void> handlePost(HttpRequest request) async {
   
-  // myStringStorage = await utf8.decoder.bind(request).join();
-  // print(myStringStorage);
- 
-//  print(json.decode(reques))
+
   String jsonString = await request.cast<List<int>>().transform(utf8.decoder).join();
-  var data = json.decode(jsonString);
-  print(data);
   
-   request.response.write(
-        // jsonEncode(
-           data
-        // ),
+  print(jsonString);
+  bool isform = false;
+  var formdata = {};
+
+  for(int i = 5 ; i < jsonString.length; i++){
+      
+      if(jsonString.substring(i-5, i) == "name="){
+          isform = true;
+          i++;
+          int start = i;
+          while( i < jsonString.length && !((jsonString[i-1] == '"' && jsonString[i] == '\r') || (jsonString[i-1] == '"' && jsonString[i] ==';')) ){
+            i++;
+          }
+          int end = i-1;
+          String key = jsonString.substring(start, end);
+          while( i < jsonString.length && !(jsonString[i-1] == '\n' && jsonString[i] == '\r')){
+            i++;
+          }
+          String value = "";
+
+          if( i < jsonString.length ){
+            i+=2;
+            start = i;
+            while(jsonString[i] != '\r'){
+              i++;
+            }
+            end = i;
+            value = jsonString.substring(start, end);
+            
+          }
+          formdata[key] = value;
+          
+
+      }
+  }
+  
+  if(!isform){
+    request.response.write(
+          jsonString
       );
+  }
+
+  request.response.write(
+      formdata
+  );
+
+  await request.response.close();
 
 
-
-
-    await request.response.close();
-
-  // request.response
-  //   ..write('Got it. Thanks.')
-  //   ..close();
-  // print(request.response);
-  // print(request.response.toString());
 }
 
 void handleDefault(HttpRequest request) {
